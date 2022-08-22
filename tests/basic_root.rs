b07ff4a69e79fe8b48583ff0d5dbea5abd19a343
sun-jacobi <sun1011jacobi@gmail.com>
2022-08-22 13:37:09 +0900

    Testing

--- tests/basic_root.rs
+++ tests/basic_root.rs
@@ -3,51 +3,27 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(blogos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+
+
 use core::panic::PanicInfo;
+
 use blogos::println;
+
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
-
-    loop {}
+    loop{}
 }
 
 
-#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    blogos::test_panic_handler(info)
+    blogos::test_panic_handler(info)   
 }
 
 
 #[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
